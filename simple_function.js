db2236c998514632fa8883a7e42783cd946ffff7
Jennifer Roblero <jenniferroblero@Jennifers-MacBook-Pro.local>
2023-05-02 13:40:13 -0400

    comit

---
+++ simple_function.js
@@ -0,0 +1,13 @@
+
+
+function favoriteThings() {
+    let name = Jamys;
+    let favMovie = Ponyo;
+    let favFood = Fries;
+
+    console.log( " My name is " + name +
+     " I have been obssesed with the movie" + favMovie + " and I really enjoy "
+     + favFood + "with cold ketchup"
+    );
+    
+}
