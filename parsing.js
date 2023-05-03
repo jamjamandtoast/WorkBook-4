9270c0d677be201a7f64134dd074ee621ac35c69
Jennifer Roblero <jenniferroblero@Jennifers-MacBook-Pro.local>
2023-05-03 13:08:29 -0400

    edits

---
+++ parsing.js
@@ -0,0 +1,17 @@
+"use strict"
+
+/* let name = "Brenda Kaye";
+
+function indexOf() {
+    let first = name.slice(0,6);
+    let last = name.slice(7,10);
+ console.log("first name: " + first + " Last name: " + last);
+}
+indexOf() */
+
+function parseAndDisplayName() {
+    parseAndDisplayName("Brenda Kaye");
+    parseAndDisplayName("Cher");
+    parseAndDisplayName("Dana Lynn Wyatt");
+}
+
