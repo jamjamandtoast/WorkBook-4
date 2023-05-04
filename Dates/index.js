6a9bdac36fdebd29e4ef0bc674b364fc4b8ba9e6
Jams Jams <yarinyroblero@Jamyss-iMac.local>
2023-05-04 10:21:33 -0400

    dates edits to complete

--- Dates/index.js
+++ Dates/index.js
@@ -1,2 +1,14 @@
 "use strict"
-const
+const dateHere = document.getElementById("dateHere");
+const dateLabel = document.getElementById("dateLabel");
+const dateOutput = document.getElementById("dateOutput");
+const convertBtn = document.getElementById("convertBtn");
+
+window.onload.onload = init;
+
+function init() {
+    convertBtn.onclick = onconvertBtnClicked;
+}
+function convertBtnClicked(){
+    console.log('clicked');
+}
